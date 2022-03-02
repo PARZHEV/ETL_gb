7679b0c8abddbe3069aa8299530a6cd5102eccb3
PARZHEV <88132441+PARZHEV@users.noreply.github.com>
2022-03-02 22:11:35 +0300

    Add files via upload
    Сделал базу в целевой БД. Создал Data Vault.  Везде даг ссылается на SQL. Дальше использовал прошлые уроки и перенес данные питоном из одной базы в другую. 
    А вот как заполнять Data Vault базу? Я думаю надо  опять создать sql файлик там писать запросы под вид новой базы данных и заполнять. Правильно?
    по примеру хабов и линков "source_system string" STRING нет же в Postgre такого типа? А так же datetime тоже. это я туплю или в методичке под другой синтаксис?
    Я использую дя написания SQL DBeaver. Правильно? Потому что эксперементировать не пойму как через airflow dags.
    Кстати по первому дагу так и не допер почему ошибка
    psycopg2.OperationalError: could not connect to server: Connection refused
    	Is the server running on host "db2" (172.18.0.5) and accepting
    	TCP/IP connections on port 5433?
    
    Не могу понять механику появления dag'ов в airflow просто сохранять надо dag в Visioи все? Иногда не появляются((

---
+++ dropifexists.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+	orderid int4 NOT NULL,
+	orderdate date NULL,
+	orderstatus varchar(1) NULL,
+	orderpriority varchar(15) NULL,
+	clerk varchar(15) NULL,
+	CONSTRAINT orders_pk PRIMARY KEY (orderid)
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+	productid int4 NOT NULL,
+	productname varchar(55) NULL,
+	producttype varchar(25) NULL,
+	productsize int4 NULL,
+	retailprice numeric(15,2) NULL,
+	CONSTRAINT products_pk PRIMARY KEY (productid)
+);
+
+DROP TABLE IF EXISTS suppliers;
+CREATE TABLE suppliers (
+	supplierid int4 NOT NULL,
+	suppliername varchar(25) NULL,
+	address varchar(40) NULL,
+	phone varchar(15) NULL,
+	balance numeric(15,2) NULL,
+	descr varchar(101) NULL,
+	CONSTRAINT supplier_pk PRIMARY KEY (supplierid)
+);
+DROP TABLE IF EXISTS orderdetails;
+CREATE TABLE orderdetails (
+	orderid int4 NOT NULL,
+	productid int4 NOT NULL,
+	unitprice numeric(15,2) NULL,
+	quantity numeric(15,2) NULL,
+	discount numeric(15,2) NULL,
+	CONSTRAINT orderdetails_pk PRIMARY KEY (orderid, productid),
+	CONSTRAINT orderdetails_fk FOREIGN KEY (orderid) REFERENCES public.orders(orderid),
+	CONSTRAINT orderdetails_fk_1 FOREIGN KEY (productid) REFERENCES public.products(productid)
+);
+DROP TABLE IF EXISTS productsuppl;
+CREATE TABLE productsuppl (
+	productid int4 NOT NULL,
+	supplierid int4 NOT NULL,
+	qty int4 NULL,
+	supplycost numeric(15,2) NULL,
+	descr varchar(199) NULL,
+	CONSTRAINT productsuppl_pk PRIMARY KEY (productid, supplierid),
+	CONSTRAINT productsuppl_fk FOREIGN KEY (productid) REFERENCES public.products(productid),
+	CONSTRAINT productsuppl_fk_1 FOREIGN KEY (supplierid) REFERENCES public.suppliers(supplierid)
+);
